df8b42af50bc90ffd0110343fe89752680d364a9
kennedynguyen1 <kennedynguyen@ucsb.edu>
2025-01-29 20:58:29 -0800

    fixed hydration error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,20 @@
 import './globals.css'
+import type { Metadata } from 'next'
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: 'SwipeRank',
+  description: 'The ultimate applicant ranking tool for clubs and organizations',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body className="--background text-foreground">{children}</body>
+      <head />
+      <body>{children}</body>
     </html>
-  );
+  )
 }
